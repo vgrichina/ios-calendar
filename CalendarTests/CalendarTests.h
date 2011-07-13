173a4e66e2bf1beaba198183a14e47f9ca0ce279
Vladimir Grichina <vgrichina@componentix.com>
2011-07-13 22:00:29 +0300

    Initial Commit

---
+++ CalendarTests/CalendarTests.h
@@ -0,0 +1,13 @@
+//
+//  CalendarTests.h
+//  CalendarTests
+//
+//  Created by Vladimir Grichina on 13.07.11.
+//  Copyright 2011 Componentix. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalendarTests : SenTestCase
+
+@end
